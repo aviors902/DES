a766453d2cf091ce56ec784700bcb0c37e2bf1c7
aviors902 <mguisard@outlook.com>
2024-05-14 20:49:59 +1000

    Update testing.py

--- testing.py
+++ testing.py
@@ -28,63 +28,72 @@ perm_choice_inverse02 = [14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10, 23, 19, 12,
 # The expansion permutation for increasing Rn from 32 bits to 48
 expansion = [32, 1, 2, 3, 4, 5, 4, 5, 6, 7, 8, 9, 8, 9, 10, 11, 12, 13, 12, 13, 14, 15, 16, 17, 16, 17, 18, 19, 20, 21, 20, 21, 22, 23, 24, 25, 24, 25, 26, 27, 28, 29, 28, 29, 30, 31, 32, 1]
 
-# The S-Boxes are written as 2d Arrays
-sbox_1 = [
-    [14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7],
-    [0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8],
-    [4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0],
-    [15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13]
-]
+def sbox_Permutation(sbox_input):
+    # The S-Boxes are written as 2d Arrays
+    sbox_1 = [
+        [14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7],
+        [0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8],
+        [4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0],
+        [15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13]
+    ]
 
-sbox_2 = [
-    [15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10],
-    [3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5],
-    [0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15],
-    [13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9]
-]
+    sbox_2 = [
+        [15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10],
+        [3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5],
+        [0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15],
+        [13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9]
+    ]
 
-sbox_3 = [
-    [10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8],
-    [13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1],
-    [13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7],
-    [1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12]
-]
+    sbox_3 = [
+        [10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8],
+        [13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1],
+        [13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7],
+        [1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12]
+    ]
 
-sbox_4 = [
-    [7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15],
-    [13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9],
-    [10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4],
-    [3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14]
-]
+    sbox_4 = [
+        [7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15],
+        [13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9],
+        [10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4],
+        [3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14]
+    ]
 
-sbox_5 = [
-    [2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9],
-    [14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6],
-    [4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14],
-    [11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3]
-]
+    sbox_5 = [
+        [2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9],
+        [14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6],
+        [4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14],
+        [11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3]
+    ]
 
-sbox_6 = [
-    [12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11],
-    [10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8],
-    [9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6],
-    [4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13]
-]
+    sbox_6 = [
+        [12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11],
+        [10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8],
+        [9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6],
+        [4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13]
+    ]
 
-sbox_7 = [
-    [4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1],
-    [13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6],
-    [1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2],
-    [6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12]
-]
+    sbox_7 = [
+        [4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1],
+        [13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6],
+        [1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2],
+        [6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12]
+    ]
 
-sbox_8 = [
-    [13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7],
-    [1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2],
-    [7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8],
-    [2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11]
-]
-
+    sbox_8 = [
+        [13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7],
+        [1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2],
+        [7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8],
+        [2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11]
+    ]
+    sbox_output_1 = sbox(sbox_1, sbox_input[:6])
+    sbox_output_2 = sbox(sbox_2, sbox_input[6:12])
+    sbox_output_3 = sbox(sbox_3, sbox_input[12:18])
+    sbox_output_4 = sbox(sbox_4, sbox_input[18:24])
+    sbox_output_5 = sbox(sbox_5, sbox_input[24:30])
+    sbox_output_6 = sbox(sbox_6, sbox_input[30:36])
+    sbox_output_7 = sbox(sbox_7, sbox_input[36:42])
+    sbox_output_8 = sbox(sbox_8, sbox_input[42:])
+    sbox_output = sbox_output_1 + sbox_output_2 + sbox_output_3 + sbox_output_4 + sbox_output_5 + sbox_output_6 + sbox_output_7 + sbox_output_8
 
 
 # A function to split blocks of text in half, returning the 2 halves as a tuple
@@ -145,33 +154,21 @@ def encrypt(plaintext, key):
 
     # This for loop is the 17 Fiestel rounds taken in DES encryption
     for n in range(1, 17):
-        #print(n)                                                                       #Debugging Print statement - Just used to indicate which iteration is being performed
+        #print(n)                                                                        Debugging Print statement - Just used to indicate which iteration is being performed
 
         # Generating the new encryption key (Rotate c1 and d1 to the left by 1, join them and then permute)
         new_Left = old_Right
         c1 = rotate(c1, key_shifts[n-1], 'l')
         d1 = rotate(d1, key_shifts[n-1], 'l')
         operation_key = permute((c1+d1), perm_choice02)
-
-        #print("key: ", operation_key, " - size: ", len(operation_key))                  # Debugging print statement
-
+        #print("key: ", operation_key, " - size: ", len(operation_key))                 # Debugging print statement
         #Expanding the old right from 32-bits to 48-bits to XOR it with the encryption key
         old_Right = permute(old_Right, expansion)
         # Permuting based on the function of (L(n-1) XOR (Sbox Output of R(n-1) XOR Kn))
         sbox_input = binary_xor(old_Right, operation_key)
-        #sbox_input = sbox_input.zfill(48)                                                # Ensure sbox_inputs is exactly 48 bits long
-
-        #print("sbox inputs: ", sbox_input, " - size: ", len(sbox_input))                # Debugging print statement
-
-        sbox_output_1 = sbox(sbox_1, sbox_input[:6])
-        sbox_output_2 = sbox(sbox_2, sbox_input[6:12])
-        sbox_output_3 = sbox(sbox_3, sbox_input[12:18])
-        sbox_output_4 = sbox(sbox_4, sbox_input[18:24])
-        sbox_output_5 = sbox(sbox_5, sbox_input[24:30])
-        sbox_output_6 = sbox(sbox_6, sbox_input[30:36])
-        sbox_output_7 = sbox(sbox_7, sbox_input[36:42])
-        sbox_output_8 = sbox(sbox_8, sbox_input[42:])
-        sbox_output = sbox_output_1 + sbox_output_2 + sbox_output_3 + sbox_output_4 + sbox_output_5 + sbox_output_6 + sbox_output_7 + sbox_output_8
+        #sbox_input = sbox_input.zfill(48)                                              # Ensure sbox_inputs is exactly 48 bits long
+        #print("sbox inputs: ", sbox_input, " - size: ", len(sbox_input))               # Debugging print statement
+        sbox_output = sbox_Permutation(sbox_input)
         # p is the final permutation of the right hand side before XORing it with the old Left hand side
         p = permute(sbox_output, permutation_p)
         # The new_Right (Rn) is formulated by performing an XOR operation on the old left (Ln-1) and the permuted s-box function output
@@ -191,7 +188,7 @@ def encrypt(plaintext, key):
         else: 
             readable_ciphertext += " " + final_permutation[i]
 
-    return readable_ciphertext, operation_key                                         # Returning a tuple of 2 objects: The Binary String containing the encrypted message, followed by the final encryption key used (So the text can be decrypted)
+    return readable_ciphertext, operation_key                                           # Returning a tuple of 2 objects: The Binary String containing the encrypted message, followed by the final encryption key used (So the text can be decrypted)
 
 
 def decrypt(ciphertext, decryption_key):  
@@ -206,18 +203,8 @@ def decrypt(ciphertext, decryption_key):
     for d in range(1, 17):
 
         new_Right = permute(old_Right, permutation_p)
-        new_Right = permute(old_Right, expansion)
-
-        sbox_input = binary_xor(old_Right, operation_Key)
-        sbox_output_1 = sbox(sbox_1, sbox_input[:6])
-        sbox_output_2 = sbox(sbox_2, sbox_input[6:12])
-        sbox_output_3 = sbox(sbox_3, sbox_input[12:18])
-        sbox_output_4 = sbox(sbox_4, sbox_input[18:24])
-        sbox_output_5 = sbox(sbox_5, sbox_input[24:30])
-        sbox_output_6 = sbox(sbox_6, sbox_input[30:36])
-        sbox_output_7 = sbox(sbox_7, sbox_input[36:42])
-        sbox_output_8 = sbox(sbox_8, sbox_input[42:])
-        sbox_output = sbox_output_1 + sbox_output_2 + sbox_output_3 + sbox_output_4 + sbox_output_5 + sbox_output_6 + sbox_output_7 + sbox_output_8
+        new_Right = permute(new_Right, expansion)
+        sbox_output = sbox_Permutation(new_Right)
 
         new_Right = binary_xor(sbox_output, old_Left)
         old_Right = new_Right
@@ -240,8 +227,7 @@ def decrypt(ciphertext, decryption_key):
 
 def main():
     # The plaintext message "0123456789ABCDEF", converted from hex to binary
-    #plaintext = "00000001 00100011 01000101 01100111 10001001 10101011 11001101 11101111"
-    plaintext = "00000000 01010100 01100101 01110011 01110100 01101001 01101110 01100111"
+    plaintext = "00000001 00100011 01000101 01100111 10001001 10101011 11001101 11101111"
 
     # The initial encryption Key K = 133457799BBCDFF1 in Hex, converted to Binary
     key = "00010011 00110100 01010111 01111001 10011011 10111100 11011111 11110001"
