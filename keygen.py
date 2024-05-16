7fe7b86ef7075ace74631269688b8a7fea0c5d5a
jarrhyp <c3355817@uon.edu.au>
2024-05-16 13:47:18 +1000

    Updated Keygen Documentation
    Testing if the push works with a smaller commit.

--- keygen.py
+++ keygen.py
@@ -5,6 +5,7 @@ key_shifts = [1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1]
 perm_choice01 = [57, 49, 41, 33, 25, 17, 9, 1, 58, 50, 42, 34, 26, 18, 10, 2, 59, 51, 43, 35, 27, 19, 11, 3, 60, 52, 44, 36, 63, 55, 47, 39, 31, 23, 15, 7, 62, 54, 46, 38, 30, 22, 14, 6, 61, 53, 45, 37, 29, 21, 13, 5, 28, 20, 12, 4]
 perm_choice02 = [14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10, 23, 19, 12, 4, 26, 8, 16, 7, 27, 20, 13, 2, 41, 52, 31, 37, 47, 55, 30, 40, 51, 45, 33, 48, 44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29, 32]
 
+# Utility functions (splitting, permutations, rotations)
 def split(text):
     half_length = len(text) // 2
     return text[:half_length], text[half_length:]
@@ -16,7 +17,7 @@ def permute(text, permutation):
 def rotate(text, v):
     return text[v:] + text[:v]
 
-    
+# Function for parsing parity bits.
 def insert_odd_parity_bit(key_56_bit):
     def odd_parity(bits):
         return '1' if bits.count('1') % 2 == 0 else '0'
