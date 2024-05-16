cda207fafd9b820847c00c68cebcd51c42aa882a
aviors902 <mguisard@outlook.com>
2024-05-16 11:20:04 +1000

    Update testing.py

--- testing.py
+++ testing.py
@@ -8,17 +8,14 @@ The purpose of this program is to demonstrate DES encryption and its implementat
 '''
 # The Initial Permutation order for the plaintext
 IP = [58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36, 28, 20, 12, 4, 62, 54, 46, 38, 30, 22, 14, 6, 64, 56, 48, 40, 32, 24, 16, 8, 57, 49, 41, 33, 25, 17, 9, 1, 59, 51, 43, 35, 27, 19, 11, 3, 61, 53, 45, 37, 29, 21, 13, 5, 63, 55, 47, 39, 31, 23, 15, 7]
-# Inverse Initial Permutation is used for the decryption process. It takes the encrypted message and arranges the bits into the same order they were placed during the very first encryption step
-IP_Inverse = [40, 8, 48, 16, 56, 24, 64, 32, 39, 7, 47, 15, 55, 23, 63, 31, 38, 6, 46, 14, 54, 22, 62, 30, 37, 5, 45, 13, 53, 21, 61, 29, 36, 4, 44, 12, 52, 20, 60, 28, 35, 3, 43, 11, 51, 19, 59, 27, 34, 2, 42, 10, 50, 18, 58, 26, 33, 1, 41,9, 49, 17, 57, 25]
-# The Final Permutation order for the plaintext
+# The Final Permutation order for the plaintext - The inverse of the Initial Permutation
 FP = [40, 8, 48, 16, 56, 24, 64, 32, 39, 7, 47, 15, 55, 23, 63, 31, 38, 6, 46, 14, 54, 22, 62, 30, 37, 5, 45, 13, 53, 21, 61, 29, 36, 4, 44, 12, 52, 20, 60, 28, 35, 3, 43, 11, 51, 19, 59, 27, 34, 2, 42, 10, 50, 18, 58, 26, 33, 1, 41, 9, 49, 17, 57, 25]
 # Permutation P is the permutation made at the end of each fiestel round
 permutation_p = [16, 7, 20, 21, 29, 12, 28, 17, 1, 15, 23, 26, 5, 18, 31, 10, 2, 8, 24, 14, 32, 27, 3, 9, 19, 13, 30, 6, 22, 11, 4, 25]
 # The Schedule of Left-Shifts for Key Permutation
 key_shifts = [1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1]
 # Master Key Permutation choices for their first permutations. The master key gets split into 2 keys: C0 and D0
-perm_choice01_C0 = [57, 49, 41, 33, 25, 17, 9, 1, 58, 50, 42, 34, 26, 18, 10, 2, 59, 51, 43, 35, 27, 19, 11, 3, 60, 52, 44, 36]
-perm_choice01_D0 = [63, 55, 47, 39, 31, 23, 15, 7, 62, 54, 46, 38, 30, 22, 14, 6, 61, 53, 45, 37, 29, 21, 13, 5, 28, 20, 12, 4]
+perm_choice01 = [57, 49, 41, 33, 25, 17, 9, 1, 58, 50, 42, 34, 26, 18, 10, 2, 59, 51, 43, 35, 27, 19, 11, 3, 60, 52, 44, 36, 63, 55, 47, 39, 31, 23, 15, 7, 62, 54, 46, 38, 30, 22, 14, 6, 61, 53, 45, 37, 29, 21, 13, 5, 28, 20, 12, 4]
 # Permutation Choice 2 extracts 48 bits for a subkey which is used in each step of the encryption process
 perm_choice02 = [14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10, 23, 19, 12, 4, 26, 8, 16, 7, 27, 20, 13, 2, 41, 52, 31, 37, 47, 55, 30, 40, 51, 45, 33, 48, 44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29, 32]
 # Permutation choice inverse01 is the inverse initial permutation of the encryption keys, used for decryption
@@ -159,8 +156,8 @@ def encryptDES0(plaintext, key):
     old_Left, old_Right = split(ciphertext)
 
     key = key.replace(" ", "")
-    c1 = permute(key, perm_choice01_C0)
-    d1 = permute(key, perm_choice01_D0)
+    keyPlus = permute(key, perm_choice01)
+    c1, d1 = split(keyPlus)
   
     keys = [None] * 16
     for k in range(0, 16):
@@ -226,8 +223,8 @@ def decryptDES0(ciphertext, key):
     old_Left, old_Right = split(ciphertext)
 
     key = key.replace(" ", "")
-    c1 = permute(key, perm_choice01_C0)
-    d1 = permute(key, perm_choice01_D0)
+    keyPlus = permute(key, perm_choice01)
+    c1, d1 = split(keyPlus)
 
     keys = [None] * 16
     for k in range(0, 16):
@@ -256,7 +253,7 @@ def decryptDES0(ciphertext, key):
         old_Right = new_Right
 
     # After 16 rounds of permutations, the final permutation FP is performed
-    final_permutation = permute(new_Left + new_Right, IP_Inverse)
+    final_permutation = permute(new_Left + new_Right, FP)
     readable_ciphertext = ""
     # A small loop just breaking the binary string into 8-bit sections
     for i in range(len(final_permutation)):
